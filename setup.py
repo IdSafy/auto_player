8a1c0fafb404d9c69fe66c973f9a4110bdd305a9
IdSaf <idolarod1@gmail.com>
2021-03-01 21:32:16 +0300

    chore: changed cli command name to auto-player for better typeablity
    Actually there are so many commands staring with auto. I'll just add some aliases into bashrc manually

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        auto_player=auto_player.cli:cli
+        auto-player=auto_player.cli:cli
     ''',
     data_files=[
       ('/etc/bash_completion.d/', ['extra/completion_script/auto_player']),
